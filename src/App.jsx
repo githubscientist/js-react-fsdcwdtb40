d174e61d0ab6fe9f3b0ce083f5423b71fb3b6ca4
githubscientist <sathish.datascientist@gmail.com>
2026-02-09 18:55:18 +0530

    multiple components + passing data as props

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+function Header({ name }) {
+
+  // props -> properties
+  // data from the parent component
+  // props -> object (javascrpt)
+  /*
+    props = {
+      name: 'sathish'
+    }
+  */
+
+  return (
+    <>
+      <h1>Hello, {name.slice(0, 1).toUpperCase() + name.slice(1,)}!</h1>
+    </>
+  )
+}
+
 function App() {
-  return <h1>Hello, World!</h1>
+  let name = 'sathish';
+
+  return (
+    <>
+      <Header name={name} />
+    </>
+  )
 }
 
 export default App;
