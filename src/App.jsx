e77180df45446a30b0975250d1294dad4ce398b1
githubscientist <sathish.datascientist@gmail.com>
2026-02-11 20:13:18 +0530

    rendering lists from arrays

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,23 @@
-import { useState } from "react";
-
-/*
-  complex state:
-
-  counts = {
-    count: 6,
-    history: ['I', 'I', 'D']
-  }
-*/
-
 const App = () => {
-
-  let [values, setValues] = useState({
-    count: 1,
-    history: []
-  });
-
-  const handleIncrease = () => {
-    if (values.count < 10) {
-      setValues({ count: values.count + 1, history: [...values.history, 'I'] });
-    }
-  }
-
-  const handleDecrease = () => {
-    if (values.count > 1) {
-      setValues({
-        count: values.count - 1,
-        history: [
-          ...values.history,
-          'D'
-        ]
-      })
-    }
-  };
-
-  const handleReset = () => {
-    setValues({
-      count: 1,
-      history: [
-        ...values.history,
-        'R'
-      ]
-    });
-  };
-
-  console.log(values);
+  let recipes = [
+    'Biryani',
+    'Pulao',
+    'Fried Rice',
+    'Noodles',
+    'Pasta',
+  ]
 
   return (
     <>
-      <button onClick={handleDecrease}>-</button>
-      &nbsp;&nbsp;<span><strong>{values.count}</strong></span>&nbsp;&nbsp;
-      <button onClick={handleIncrease}>+</button>
-      &nbsp;&nbsp;
-      <button onClick={handleReset}>Reset</button>
+      <h1>Recipes</h1>
+      <ul>
+        {
+          recipes
+            .map((recipe, index) => (
+              <li key={index}>{recipe}</li>
+            ))
+        }
+      </ul>
     </>
   )
 }
