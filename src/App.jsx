0a153608fb3a4ff538c4262a557008b098b7f0eb
githubscientist <sathish.datascientist@gmail.com>
2026-02-12 20:02:01 +0530

    fetch call

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import Login from "./components/Login";
+import { useState } from "react";
 
 const App = () => {
+
+  const [todos, setTodos] = useState([]);
+
+  const fetchTodos = () => {
+    fetch('https://698dd177b79d1c928ed6a345.mockapi.io/todos')
+      .then(response => {
+        return response.json();
+      })
+      .then(response => {
+        setTodos(response);
+      })
+  }
+
+  console.log(todos);
+
   return (
     <>
-      <Login />
+      <button onClick={fetchTodos}>Fetch Todos</button>
+      <h1>Todos</h1>
+      <ul>
+        {
+          todos
+            .map(todo => (
+              <li key={todo.id}>{todo.title}</li>
+            ))
+        }
+      </ul>
     </>
   )
 }
