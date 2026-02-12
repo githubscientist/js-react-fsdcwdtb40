fd568dcd4f31a07c1ae9d3f59e8abdc765667bd3
githubscientist <sathish.datascientist@gmail.com>
2026-02-12 20:35:15 +0530

    notes updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,18 @@ import { useEffect, useState } from "react";
     - It's a react hook.
     - It allows us to perform side effects in our components.
     - examples: fetching data, setting up a subscription, and manually changing the DOM in React components.
+
+  useEffect is used for the following scenarios:
+
+    1. Run a function or side effect after the component renders for the first time.
+    2. Run a function or side effect after every render of the component.
+    3. Run a function or side effect only when specific state variables change.
 */
 
 const App = () => {
 
   const [likes, setLikes] = useState(0);
   const [dislikes, setDislikes] = useState(0);
-
-  /*
-    1. run a function after a component is rendered.
-    2. run a function after a component is rendered and whenever a particular state changes.
-    3. run a function after a component is rendered and whenever any state changes.
-  */
 
   useEffect(() => {
     console.log('runs after first render and for every state changes');
