3f5741c49ba9312cd3b67d005dc5772cacf0aa41
githubscientist <sathish.datascientist@gmail.com>
2026-02-16 20:26:26 +0530

    linking the routes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,19 @@
 const Home = () => {
     return (
-        <>
-            <h1 className="text-4xl p-4">Home</h1>
-        </>
+        <div className="p-4">
+            <h1 className="text-3xl p-4">
+                Welcome to my React Router App
+            </h1>
+
+            <p className="p-4">
+                This is the home page of my React Router App. You can navigate to the register and login pages using the links in the header.
+            </p>
+
+            <div className="p-4 flex flex-row gap-5">
+                <a className="hover:text-red-700 hover:border hover: border-2 hover: p-2" href="/register" >Register</a>
+                <a className="hover:text-red-700 hover:border hover: border-2 hover: p-2" href="/login">Login</a>
+            </div>
+        </div>
     )
 }
 
