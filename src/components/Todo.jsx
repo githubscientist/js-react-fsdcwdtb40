feb70b25f7ad008dc19bd288af07d89b25550175
githubscientist <sathish.datascientist@gmail.com>
2026-02-18 19:08:28 +0530

    axios call using useEffect

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
 const Todo = () => {
 
     const { id } = useParams();
+    const [todo, setTodo] = useState({});
 
-    console.log(id);
+    const fetchTodo = async () => {
+        const response = await axios.get(`https://698dd177b79d1c928ed6a345.mockapi.io/todos/${id}`);
+        setTodo(response.data);
+    }
+
+    useEffect(() => {
+        fetchTodo();
+    }, [id]);
+
+    useEffect(() => {
+        console.log(todo);
+    }, [todo]);
 
     return (
         <div className="mt-4">
