3372c272c7fddb757d3392cc1f5bbd87ec2357a4
githubscientist <sathish.datascientist@gmail.com>
2026-02-23 18:37:43 +0530

    basic cleanup --done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,6 @@
-import { useDispatch, useSelector } from "react-redux";
-import { increment, selectCount } from "./redux/features/counter/counterSlice";
-
 const App = () => {
-  const count = useSelector(selectCount);
-  const dispatch = useDispatch();
-
-  const handleIncrease = () => {
-    dispatch(increment());
-  }
-
   return (
-    <>
-      <h1>Count: {count}</h1>
-      <button onClick={handleIncrease}>Increase</button>
-    </>
+    <div>App</div>
   )
 }
 
